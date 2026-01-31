4ed3331a36429cedbbe0e55f8ab414284e6b0c1c
Brainly User <user@example.com>
2026-01-31 19:01:20 +0530

    initial commit

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model } from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/brainly");
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  type: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  userId: { type: mongoose.Types.ObjectId, ref: "UserModel", required: true },
+});
+
+export const UserModel = model("UserModel", UserSchema);
+export const ContentModel = model("ContentModel", ContentSchema);
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import express from "express";
+import { ContentModel, UserModel } from "./db.js";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config.js";
+import { userMiddleware } from "./middlewares.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+  const userExists = await UserModel.findOne({
+    username,
+    password,
+  });
+
+  if (userExists) {
+    const token = jwt.sign(
+      {
+        id: userExists._id,
+      },
+      JWT_PASSWORD,
+    );
+
+    res.json({ token });
+  } else {
+    res.status(403).json({ message: "Invalid credentials" });
+  }
+});
+
+app.post("/api/v1/signup", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    await UserModel.create({
+      username: username,
+      password: password,
+    });
+
+    res.json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(411).json({ message: "username already exists" });
+  }
+});
+app.post("/api/v1/content",userMiddleware, (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+
+    ContentModel.create({
+        link,
+        type,
+        // @ts-ignore
+        userId: req.userId,
+        tags:[],
+    })
+
+     res.json({message: "content created"})
+});
+app.get("/api/v1/content",userMiddleware, (req, res) => {});
+app.delete("/api/v1/content",userMiddleware, (req, res) => {});
+app.post("/api/v1/brain/share",userMiddleware, (req, res) => {});
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
