f22f15a943c96a9a9db48ae6b9e032c2e5296eea
Brainly User <user@example.com>
2026-02-06 18:41:57 +0530

    backend completed finally

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -109,7 +109,7 @@ app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
     res.json({ message: "Link removed" });
   }
 });
-app.post("/api/v1/brain/:shareLink", async (req, res) => {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
   const hash = req.params.shareLink;
 
   const link = await LinkModel.findOne({
