004dfe96704be5645b1ced47e82f60f7824e0821
rishi9393 <justforuse2323@gmail.com>
2026-02-08 18:28:43 +0530

    started building Sidebar

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -4,42 +4,52 @@ import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 import { CreateContentModel } from "./components/CreateContentModel";
 import { useState } from "react";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   const [modelOpen, setModelOpen] = useState(true);
   return (
-    <div>
-      <CreateContentModel
-        open={modelOpen}
-        onClose={() => {
-          setModelOpen(false);
-        }}
-      />
-      <div className="justify-end flex gap-4 mt-2 ">
-        <Button
-          variant="primary"
-          text="Add Content"
-          startIcon={<PlusIcon />}
-          onclick={() => {
-            setModelOpen(true);
+    <div className="border-gray-200">
+      <div className="">
+        <Sidebar />
+      </div>
+      <div className="ml-72 min-h-screen p-4 bg-gray-200 border-b border-gray-100">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
           }}
         />
-        <div className="mr-2 ">
-          <Button variant="secondary" text="Share" startIcon={<ShareIcon />} />
+        <div className="justify-end flex gap-4 mt-2 ">
+          <Button
+            variant="primary"
+            text="Add Content"
+            startIcon={<PlusIcon />}
+            onclick={() => {
+              setModelOpen(true);
+            }}
+          />
+          <div className="mr-2 ">
+            <Button
+              variant="secondary"
+              text="Share"
+              startIcon={<ShareIcon />}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="flex gap-4 mt-4">
-        <Card
-          title="Sample YouTube Video"
-          link="https://www.youtube.com/watch?v=JUeIGphgmcU"
-          type="youtube"
-        />
-        <Card
-          title="twitter"
-          link="https://twitter.com/eddiejaoude/status/2019970352412307854"
-          type="twitter"
-        />
+        <div className="flex gap-4 mt-4">
+          <Card
+            title="Sample YouTube Video"
+            link="https://www.youtube.com/watch?v=JUeIGphgmcU"
+            type="youtube"
+          />
+          <Card
+            title="twitter"
+            link="https://twitter.com/eddiejaoude/status/2019970352412307854"
+            type="twitter"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ frontend/vite-project/src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+export function Sidebar() {
+  return (
+    <div className="h-screen bg-white border-r border-gray-200 w-72 fixed left-0 top-0">hi there</div>
+  );
+}
