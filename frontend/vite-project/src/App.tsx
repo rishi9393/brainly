c3fe0226f258e90bf9cdeaf3ea24955859ea902d
rishi9393 <justforuse2323@gmail.com>
2026-02-12 13:46:55 +0530

    completed Sigin page part

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,18 +1,18 @@
-import Dashboard from "./pages/dashboard";
-import { Sigin } from "./pages/Sigin";
-import { Sigup } from "./pages/Signup";  
-import {Route,BrowserRouter,Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { Signin } from "./pages/Sigin";
+import { Sigup } from "./pages/Signup";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Sigup />} />
-      <Route path="/sigin" element={<Sigin />} />
-      <Route path="/signup" element={<Sigup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes> 
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sigup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Sigup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
